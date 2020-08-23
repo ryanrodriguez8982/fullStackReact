0504c456360874345c2de49f7fde96d3f49f80ba
Ryan Rodriguez <ryan.rodriguez8982@gmail.com>
2020-08-23 15:30:43 -0500

    Add Header and Footer Components

--- react/confusion/src/components/MainComponent.js
+++ react/confusion/src/components/MainComponent.js
@@ -1,10 +1,11 @@
-import React from 'react';
-import { Navbar, NavbarBrand } from 'reactstrap';
+import React, {Component} from 'react';
+import Header from './HeaderComponent';
 import Menu from './MenuComponent';
 import Detail from './DishdetailComponent';
+import Footer from './FooterComponent';
 import { DISHES } from '../shared/dishes';
 
-class Main extends Menu {
+class Main extends Component {
 
   constructor(props) {
     super(props);
@@ -23,16 +24,11 @@ class Main extends Menu {
   render() {
     return (
       <div>
-        <Navbar dark color="primary">
-          <div className="container">
-            <NavbarBrand href="/">
-              Ristorante Con Fusion
-          </NavbarBrand>
-          </div>
-        </Navbar>
+        <Header />
         <Menu dishes={this.state.dishes}
           onClick={(dishId) => this.onDishSelect(dishId)} />
         <Detail dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} />
+        <Footer />
       </div>
     );
   }
